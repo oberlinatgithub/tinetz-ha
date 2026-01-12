f6fea4983cffdc33cf2ea4390f9a86ed5caa6d30
oberlinatgithub <78933731+oberlinatgithub@users.noreply.github.com>
2026-01-12 16:23:56 +0100

    config_flow.py

--- custom_components/tinetz/config_flow.py
+++ custom_components/tinetz/config_flow.py
@@ -1,28 +1,49 @@
 from homeassistant import config_entries
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import voluptuous as vol
+import logging
+
 from .api import TinetzApi
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
 
 class TinetzConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        if user_input is not None:
-            api = TinetzApi(
-                user_input["username"],
-                user_input["password"]
-            )
-            await self.hass.async_add_executor_job(api.login)
+        errors = {}
 
-            return self.async_create_entry(
-                title="TINETZ",
-                data=user_input
-            )
+        if user_input is not None:
+            username = user_input["username"]
+            password = user_input["password"]
+
+            session = async_get_clientsession(self.hass)
+            api = TinetzApi(session)
+
+            try:
+                await api.login(username, password)
+
+                return self.async_create_entry(
+                    title="TINETZ",
+                    data={
+                        "username": username,
+                        "password": password,
+                    },
+                )
+
+            except Exception as err:
+                _LOGGER.exception("Unexpected error during TINETZ login")
+                errors["base"] = "cannot_connect"
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("username"): str,
-                vol.Required("password"): str
-            })
+            data_schema=vol.Schema(
+                {
+                    vol.Required("username"): str,
+                    vol.Required("password"): str,
+                }
+            ),
+            errors=errors,
         )
